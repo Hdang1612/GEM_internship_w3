da13fe42d8382c93a851c60c96eb48ef8b20db43
Tô Hải Đăng <tohaidang161@gmail.com>
2025-07-01 17:15:57 +0700

    feat : setup side menu

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const slider = document.querySelector(".slider");
 const dots = document.querySelectorAll(".dot");
+const menuIconBtn = document.querySelector(".header__menu-icon");
+const menuContent = document.querySelector(".header__menu-hidden");
+const menuCloseBtn = document.querySelector(".header__close-btn");
 
 let currentSlide = 0;
 const totalSlides = 3;
@@ -29,3 +32,30 @@ function resetInterval() {
   slideInterval = setInterval(nextSlide, 5000);
 }
 updateSlider();
+
+menuIconBtn.addEventListener("click", () => {
+  if (menuContent.classList.contains("header__menu-hidden--active")) {
+    menuContent.classList.remove("header__menu-hidden--active");
+  } else {
+    menuContent.classList.add("header__menu-hidden--active");
+    document.getElementsByTagName("body")[0].style.overflow = "hidden";
+  }
+});
+
+menuCloseBtn.addEventListener("click", function () {
+  if (menuContent.classList.contains("header__menu-hidden--active")) {
+    menuContent.classList.remove("header__menu-hidden--active");
+    document.getElementsByTagName("body")[0].style.overflow = "auto";
+  } else {
+    menuContent.classList.add("header__menu-hidden--active");
+  }
+});
+window.addEventListener("click", function (event) {
+  if (
+    !menuContent.contains(event.target) &&
+    !menuIconBtn.contains(event.target)
+  ) {
+    menuContent.classList.remove("header__menu-hidden--active");
+    document.getElementsByTagName("body")[0].style.overflow = "auto";
+  }
+});
